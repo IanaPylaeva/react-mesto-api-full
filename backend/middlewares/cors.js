950a3f3038338df362f42774105d63140dc75c8a
Iana Pylaeva <MPiY@yandex.ru>
2022-07-13 23:17:27 +0300

    15

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -12,7 +12,7 @@ const allowedCors = [
 const cors = (req, res, next) => {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE'; // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = 'GET, HEAD, PUT, PATCH, POST, DELETE'; // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
   const requestHeaders = req.headers['access-control-request-headers']; // сохраняем список заголовков исходного запроса
 
   // проверяем, что источник запроса есть среди разрешённых
