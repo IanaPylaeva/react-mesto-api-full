82c95dcf4a7416acb69f39566016e7ef2faa3fb1
Iana Pylaeva <MPiY@yandex.ru>
2022-07-13 22:07:54 +0300

    15

--- backend/app.js
+++ backend/app.js
@@ -39,7 +39,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(helmet());
 app.use(bodyParser.json());
 
-app.use(cors(allowedCors));
+app.use('*', cors(allowedCors));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
