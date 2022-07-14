2e45d6a325233b404c91f4cb694ea3ddbe235f93
Iana Pylaeva <MPiY@yandex.ru>
2022-07-14 14:41:35 +0300

    15

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,21 +4,18 @@ const validator = require('validator');
 const userSchema = new mongoose.Schema({
   name: { // у пользователя есть имя — опишем требования к имени в схеме:
     type: String, // имя — это строка
-    required: true, // оно должно быть у каждого пользователя, так что имя — обязательное поле
     minlength: 2, // минимальная длина имени — 2 символа
     maxlength: 30, // а максимальная — 30 символов
     default: 'Жак-Ив Кусто', // по умолчанию
   },
   about: {
     type: String, // это строка
-    required: true, // обязательное поле
     minlength: 2, // минимальная длина  — 2 символа
     maxlength: 30, // а максимальная — 30 символов
     default: 'Исследователь', // по умолчанию
   },
   avatar: {
     type: String, // это строка
-    required: true, // обязательное поле
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png', // по умолчанию
     validate: validator.isURL,
   },
