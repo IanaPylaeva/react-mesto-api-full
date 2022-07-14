d815b15cc172f8ed9b8baf40d4891d93975f3fdd
Iana Pylaeva <MPiY@yandex.ru>
2022-07-14 16:13:46 +0300

    15

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -34,19 +34,6 @@ function App() {
   const [infoTooltip, setInfoTooltip] = React.useState(false);
   const [isImagePopupOpen, setIsImagePopupOpen] = React.useState(false);
 
-  /* Одновременное получение данных пользователя и карточек */
-  useEffect(() => {
-    if (isLoggedIn) {
-    Promise.all([api.getUserData(), api.getInitialCards()])
-      .then((res) => {
-        setCurrentUser(res[0]);
-        setCards(res[1]);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }}, [isLoggedIn]);
-
   /* Регистрация */
   function register(email, password) {
     auth.registerUser(email, password).then(() => {
@@ -87,13 +74,28 @@ function App() {
       })
     }
   }, []);
-    
+  
   useEffect(() => {
     if (isLoggedIn) {
       navigate('/');
-    }
+      Promise.all([api.getUserData(), api.getInitialCards()])
+      .then((res) => {
+        setCurrentUser(res[0]);
+        setCards(res[1]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    };
   }, [isLoggedIn, navigate]);
   
+  /* Одновременное получение данных пользователя и карточек */
+  /*
+  useEffect(() => { 
+    if (isLoggedIn) {
+
+  }, [isLoggedIn]);
+  */
 
   function handleEditProfileClick(){
     setIsEditProfilePopupOpen(true);
